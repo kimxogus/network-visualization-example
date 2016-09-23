e649d162467374f5205dfa0c16e419d42581447f
Taehyun Kim <kgyoo8232@gmail.com>
2016-09-24 01:22:11 +0900

    add dependencies for cytoscape

--- app/cytoscape-network.js
+++ app/cytoscape-network.js
@@ -2,3 +2,14 @@
  * Created by Taehyun on 2016-09-23.
  */
 import './common'
+
+import cytoscape from 'cytoscape'
+import 'cytoscape-qtip'
+import panzoom from 'cytoscape-panzoom'
+import forcelayout from 'cytoscape-ngraph.forcelayout'
+
+import genData from './data'
+
+
+panzoom(cytoscape);
+forcelayout(cytoscape);
