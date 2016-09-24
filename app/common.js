48d4b819f95bea14c1d8c0d0f93340683e4b388a
kimxogus <kgyoo8232@gmail.com>
2016-09-25 00:20:25 +0900

    add gh-pages

--- app/common.js
+++
@@ -1,9 +0,0 @@
-/**
- * Created by Taehyun on 2016-09-23.
- */
-
-import 'bootstrap/dist/css/bootstrap.css'
-import '../css/style.css'
-
-import 'jquery'
-import 'lodash'
--- app/main.js
+++
@@ -1,5 +0,0 @@
-/**
- * Created by Taehyun on 2016-09-23.
- */
-import './common'
-
